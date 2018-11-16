fb883c8243b90e5c05e5cb43b53317c505e675a2
Marco Aime <marco.aime@techedgegroup.com>
2018-11-16 15:50:53 +0100

    Revert "Fist Integration"
    This reverts commit df943681564aa1f69ebde0755ca8f2a7e0566a7c.

--- read_csv_func.R
+++
@@ -1,8 +0,0 @@
-# Set options
-data_folder_path = "./data/"
-
-
-read_file = function(file_name, sep=",") {
-  file_path = paste(data_folder_path, file_name, sep="")
-  read.csv(file_path, header = TRUE, sep=sep)
-}
